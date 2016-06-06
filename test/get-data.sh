80b3754f94d4daa567d1f328c3fd1f755232957a
daler <dalerr@niddk.nih.gov>
2016-06-06 10:37:07 -0400

    notify if example data dir exists

--- test/get-data.sh
+++ test/get-data.sh
@@ -8,4 +8,6 @@ if [ ! -e pasilla ]; then
     wget http://helix.nih.gov/~dalerr/lcdb-workflows-data/pasilla.tar
     tar -xvf pasilla.tar
     rm pasilla.tar
+else
+    echo "'pasilla' directory already exists"
 fi
