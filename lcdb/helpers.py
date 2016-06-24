83cb346e4f9053999ea9dc93f57fcec5b4ec563d
daler <dalerr@niddk.nih.gov>
2016-06-24 11:07:25 -0400

    use helpers module

--- lcdb/helpers.py
+++ lcdb/helpers.py
@@ -1,3 +1,5 @@
+import os
+import pandas
 import yaml
 from jsonschema import validate, ValidationError
 
@@ -10,3 +12,81 @@ def validate_config(config, schema):
     except ValidationError as e:
         msg = '\nPlease fix %s: %s\n' % (config, e.message)
         raise ValidationError(msg)
+
+def build_wrapper_for(source_dir, wrappers_dir):
+    """
+    Returns a `wrapper_for` function to be used in a workflow.
+
+    Parameters
+    ----------
+    :source_dir: str
+        Directory of the calling snakemake workflow. Typically this is obtained
+        with the srcdir() built-in.
+    :wrappers_dir: str
+        Directory of wrappers relative to source dir
+    """
+    def wrapper_for(tool):
+        return os.path.join(source_dir, wrappers_dir, tool)
+    return wrapper_for
+
+def build_params_for(config):
+    """
+    Returns a `params_for` function to be used in a workflow.
+
+    Parameters
+    ----------
+    :config: dict
+        The global config dictionary from a workflow
+    """
+    def params_for(rule, key):
+        return  config.get('rules', {}).get(rule, {}).get('params', {}).get(key, '')
+    return params_for
+
+
+def build_threads_for(config):
+    """
+    Returns a `threads_for` function to be used in a workflow.
+
+    Parameters
+    ----------
+    :config: dict
+        The global config dictionary from a workflow
+    """
+    def threads_for(rule):
+        return  config.get('rules', {}).get(rule, {}).get('threads', None)
+    return threads_for
+
+
+def workflow_helper_functions(config, source_dir, wrappers_dir):
+    """
+    One-stop-shop for building helper functions.
+
+    Parameters
+    ----------
+    :config: dict
+        The global config dictionary from a workflow
+    :source_dir: str
+        Directory of the calling snakemake workflow. Typically this is obtained
+        with the srcdir() built-in.
+    :wrappers_dir: str
+        Directory of wrappers relative to source dir
+
+    Returns
+    -------
+    wrappers_for, params_for, and threads_for functions.
+    """
+    return (
+        build_wrapper_for(source_dir, wrappers_dir),
+        build_params_for(config),
+        build_threads_for(config),
+    )
+
+
+def load_sampletable(filename):
+    """
+    Load sampletable.
+
+    TODO: validation will go here.
+    """
+    return pandas.read_table(filename, index_col=0)
+
